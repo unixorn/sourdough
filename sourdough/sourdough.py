d955ab9f72e3ed32aa3be1315cef623963d1190f
Andrey Chernih <achernykh@castlighthealth.com>
2019-03-27 14:22:18 -0700

    Use default route interface IP address when searching for tags
    Hostnames are resolving to `169.254.1.1` which is a dummy IP address
    which can not be used for searching in VSphere. Switch to using the
    default IP address from routes table of the node.

--- sourdough/sourdough.py
+++ sourdough/sourdough.py
@@ -192,13 +192,25 @@ def readKnobOrTagValue(name, connection=None, knobDirectory='/etc/knobs'):
       return None
   return None # No knobfile and we're either outside EC2/VMware or no tag either
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
 def readVirtualMachineTag(tagName):
     '''
     Read Tags / Attributes from VM
 
     :rtype: str
     '''
-    vm_ip = socket.gethostbyname(socket.gethostname())
+    vm_ip = get_ip()
 
     if vm_ip not in vmwareTags:
       vmwareTags[vm_ip] = {}
