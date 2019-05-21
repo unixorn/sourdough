4c608eb8713f24c4efbafb724a3824111fad8261
Joe Block <jpb@unixorn.net>
2019-05-21 17:28:39 -0600

    Fix indentation in get_ip()

--- sourdough/sourdough.py
+++ sourdough/sourdough.py
@@ -172,6 +172,7 @@ def readKnobOrTag(name, connection=None, knobDirectory='/etc/knobs'):
 
   return knobsCache[name]
 
+
 def readKnobOrTagValue(name, connection=None, knobDirectory='/etc/knobs'):
   '''
   Read a knob file or EC2 instance tag
@@ -212,17 +213,19 @@ def readKnobOrTagValue(name, connection=None, knobDirectory='/etc/knobs'):
       return None
   return None # No knobfile and we're either outside EC2/VMware or no tag either
 
+
 def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.255.255.255', 1))
-        IP = s.getsockname()[0]
-    except:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  try:
+    # doesn't even have to be reachable
+    s.connect(('10.255.255.255', 1))
+    IP = s.getsockname()[0]
+  except:
+    IP = '127.0.0.1'
+  finally:
+    s.close()
+  return IP
+
 
 def readVirtualMachineTag(tagName):
     '''
